8bded2d92bac664056789e54a587e629dbeaa64e
Sawertynn <sawertynn@gmail.com>
2022-04-07 10:18:31 +0200

    all parts working, before dividing into smaller funcs

--- epic_cebula.py
+++ epic_cebula.py
@@ -20,6 +20,10 @@ width, height = gui.size()
 roll = int(height / 3)
 freegame_offset = int(height / 5)
 nextgame_offset = int(width / 5.2)
+scrollbar_x = 1915
+scrollbar_up = 152
+scrollbar_down = 900
+scroll_color = 400  # RGB sum
 
 
 def launch_app():
@@ -29,13 +33,37 @@ def launch_app():
         pass
 
 
+def safe_click(target, reps=3, countdown=0):
+    if countdown > 0:
+        time.sleep(countdown)
+    for _ in range(reps):
+        pos = gui.locateOnScreen(target)
+        if pos:
+            break
+    if not pos:
+        return False
+    gui.click(gui.center(pos))
+    return True
+
+
 def locate_free_games(game_count):
+    direc = -1;
     while True:
         pos = gui.locateOnScreen('Pics/free_now.png')
         if pos:
             break
-        gui.scroll(-roll)
-    pos = gui.locateOnScreen('Pics/free_now.png')
+        gui.scroll(direc * roll)
+        if sum(gui.pixel(scrollbar_x, scrollbar_down)) > scroll_color:
+            direc = 1
+        elif sum(gui.pixel(scrollbar_x, scrollbar_up)) > scroll_color:
+            direc = -1
+    for _ in range(3):
+        pos = gui.locateOnScreen('Pics/free_now.png')
+        if pos:
+            break
+    if not pos:
+        print('HEAVY ERROR')
+        return -1, -1
     x = pos[0] + pos[2] + game_count * int(width / 5.2)
     y = pos[1] + int(height / 100)
     if gui.pixel(x, y)[2] > 128:
@@ -47,48 +75,71 @@ def locate_free_games(game_count):
 # add game to cart
 def add_to_cart(tile_pos):
     gui.click(tile_pos)
-    time.sleep(2)
-    pos = gui.locateOnScreen('Pics/add_to_cart.png')
+    for _ in range(3):
+        pos = gui.locateOnScreen('Pics/add_to_cart.png')
+        if pos:
+            break
+        if gui.locateOnScreen('Pics/view_in_chart.png'):  # already in chart
+            break
     if pos:
         gui.click(gui.center(pos))
     back = gui.locateOnScreen('Pics/back.png')
     if not back:
         back = gui.locateOnScreen('Pics/store.png')
     gui.click(back)
+    time.sleep(2)
     return bool(pos)
 
 
 # buy screen
 def buy_games():
-    order_pos = 1432, 970
-    order_color = 39, 140, 242
-    min_color = 220
-    max_color = 560
-
-    while True:
-        color = sum(gui.pixel(order_pos[0], order_pos[1]))
-        if color in range(min_color, max_color):
+    for _ in range(3):
+        pos = gui.locateOnScreen('Pics/cart.png')
+        if pos:
             break
-        time.sleep(.2)
+    if not pos:
+        return
+    gui.click(gui.center(pos))
 
-    gui.moveTo(order_pos)
-    gui.click(order_pos)
+    for _ in range(3):
+        pos = gui.locateOnScreen('Pics/checkout.png')
+        if pos:
+            break
+    if not pos:
+        return
+    gui.click(gui.center(pos))
 
-    # agree
-    agree_pos = 1250, 758
-    gui.moveTo(agree_pos)
+    for _ in range(5):
+        pos = gui.locateOnScreen('Pics/place_order.png')
+        if pos:
+            break
+    if not pos:
+        return
+    gui.click(gui.center(pos))
+
+    for _ in range(3):
+        pos = gui.locateOnScreen('Pics/agree.png')
+        if pos:
+            break
+    if not pos:
+        return
+    gui.click(gui.center(pos))
 
 
 def main():
-    game_count = 0
-    launch_app()
-    tile_pos = locate_free_games(game_count)
-    logging.info(f'{tile_pos=}')
-    while tile_pos[0] > 0:
-        add_to_cart(tile_pos)
-        game_count += 1
-        logging.info(f'gc={game_count}, pos={tile_pos}')
-        locate_free_games(game_count)
+    # game_count = 0
+    # launch_app()
+    # tile_pos = locate_free_games(game_count)
+    # logging.info(f'{tile_pos=}')
+    # while tile_pos[0] > 0:
+    #     add_to_cart(tile_pos)
+    #     game_count += 1
+    #     logging.info(f'before -- gc={game_count}, pos={tile_pos}')
+    #     tile_pos = locate_free_games(game_count)
+    #     logging.info(f'after -- gc={game_count}, pos={tile_pos}')
+    gui.hotkey('alt', 'tab')
+    buy_games()
+    subprocess.run(['taskkill', app_name], capture_output=True)
 
 
 if __name__ == '__main__':
